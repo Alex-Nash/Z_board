0fbe695bc4f94663e02acd87be7908ed689d9366
Alex-Nash <Alex_smile@inbox.ru>
2016-07-06 22:03:01 +0300

    add file

---
+++ workspace/SendSignalOnPin3And4/Debug/src/UseGPIO.d
@@ -0,0 +1,43 @@
+src/UseGPIO.o src/UseGPIO.o: ../src/UseGPIO.c \
+ ../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xparameters.h
+
+../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xparameters.h:
---
+++ workspace/SendSignalOnPin3And4/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../SendSignalOnPin3And4_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
